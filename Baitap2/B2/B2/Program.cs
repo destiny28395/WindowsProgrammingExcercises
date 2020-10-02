46a4e129e451f0a359b8cd9a9a5675b7d790e186
Bảo Long <68813160+destiny28395@users.noreply.github.com>
2020-10-02 18:35:38 +0700

    first summit

---
+++ Baitap2/B2/B2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace B2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            int a, b,tong;
+            Console.WriteLine("Enter your name:");
+            s = Console.ReadLine();
+            Console.WriteLine("Hello "+s);
+            
+        }
+    }
+}
---
+++ Baitap2/B2TInh/B2TInh/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace B2TInh
+{
+    class Program
+    {
+        public static int Cong(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Tru(int a, int b)
+        {
+            if (a > b)
+                return a - b;
+            else
+                return b - a;
+        }
+        public static int Nhan(int a, int b)
+        {
+            return a * b;
+        }
+        public static float Chia(float a, float b)
+        {
+            if (a > b)
+                return a / b;
+            else
+                return b / a;
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Enter number a:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Enter number b:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sau khi cong la:"+Cong(a, b));
+            Console.WriteLine("Sau khi tru la:" + Tru(a, b));
+            Console.WriteLine("Sau khi nhan la:" + Nhan(a, b));
+            Console.WriteLine("Sau khi chia la:" + (float)Chia(a, b));
+        }
+    }
+}
